0394bac88579952d8c70999d9e0eddbcff4fe174
p1armod <parmodrns@gmail.com>
2025-12-28 18:43:44 +0530

    Update App for Showing The charts

--- wikimedia-dashboard/src/App.js
+++ wikimedia-dashboard/src/App.js
@@ -1,23 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import EditChart from "./components/EditChart";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: 20 }}>
+      <h2>Wikimedia Live Edit Count</h2>
+      <EditChart />
     </div>
   );
 }
